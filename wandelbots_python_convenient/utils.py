43ffc716c7f69bfc9ff2f03040ea12ff16e5784d
Stefan Wagner <stefan.wagner@wandelbots.com>
2024-11-11 14:59:33 +0100

    fix: update lib to support access tokens

--- wandelbots_python_convenient/utils.py
+++ wandelbots_python_convenient/utils.py
@@ -9,30 +9,35 @@ CELL_ID = config("CELL_ID", default="cell", cast=str)
 def get_api_client() -> Instance:
     """Creates a new API client for the wandelbots API.
     """
-    # if there is basic auth required (e.g. when running this service locally)
-    basic_user = config("NOVA_USERNAME", default=None, cast=str)
-    basic_pwd = config("NOVA_PASSWORD", default=None, cast=str)
+    # if there is a token required for cloud access (e.g. when running this service locally)
+    access_token = config("NOVA_ACCESS_TOKEN", default=None)
+    basic_auth = get_basic_auth()
 
-    base_url = get_base_url(basic_user, basic_pwd)
+    base_url = get_base_url(access_token, basic_auth)
 
-    return create_instance(base_url=base_url, user=basic_user, password=basic_pwd)
+    return create_instance(base_url=base_url, access_token=access_token, basic_auth=basic_auth)
 
-def create_instance(base_url, user, password) -> Instance:
+def create_instance(base_url, access_token, basic_auth) -> Instance:
     parsed_url = urlparse(base_url)
-    if parsed_url.scheme == "https://":
-        return Instance(url=base_url, user=user, password=password)
+    if "https" in parsed_url.scheme:
+        if access_token is not None:
+            return Instance(url=base_url, access_token=access_token)
+        elif basic_auth is not None:
+            return Instance(url=base_url, user=basic_auth.username, password=basic_auth.password)
+        else:
+            raise Exception("Please provide access token or basic auth via env vars.")
     else:
         return Instance(url=base_url)
     
-def get_base_url(basic_user, basic_pwd) -> str:
+def get_base_url(access_token, basic_auth) -> str:
     # in-cluster it is the api-gateway service
     # when working with a remote instance one needs to provide the host via env variable
-    api_host = config("WANDELAPI_BASE_URL", default="api-gateway:8080", cast=str)
+    api_host = config("WANDELAPI_BASE_URL", default="api-gateway:8080")
     api_host = api_host.strip()
     api_host = api_host.replace("http://", "")
     api_host = api_host.replace("https://", "")
     api_host = api_host.rstrip("/")
-    protocol = get_protocol(api_host, basic_user, basic_pwd)
+    protocol = get_protocol(api_host, access_token, basic_auth)
     if protocol is None:
         msg = f"Could not determine protocol for host {api_host}. Make sure the host is reachable."
         raise Exception(msg)
@@ -40,24 +45,33 @@ def get_base_url(basic_user, basic_pwd) -> str:
 
 
 # get the protocol of the host (http or https)
-def get_protocol(host, basic_user, basic_pwd) -> str:
+def get_protocol(host, access_token, basic_auth) -> str:
     api = f"/api/v1/cells/{CELL_ID}/controllers"
-    auth = None
-    if basic_user is not None and basic_pwd is not None:
-        auth = HTTPBasicAuth(basic_user, basic_pwd)
+    headers = None
+    
+    if access_token is not None:
+        headers = {"Authorization": f"Bearer {access_token}"}
+        basic_auth = None
 
     try:
-        response = requests.get(f"https://{host}{api}", auth=auth, timeout=5)
+        response = requests.get(f"https://{host}{api}", headers=headers, auth=basic_auth, timeout=5)
         if response.status_code == 200:
             return "https://"
     except requests.RequestException:
         pass
 
     try:
-        response = requests.get(f"http://{host}{api}", auth=auth, timeout=5)
+        response = requests.get(f"http://{host}{api}", timeout=5)
         if response.status_code == 200:
             return "http://"
     except requests.RequestException:
         pass
 
+    return None
+
+def get_basic_auth() -> dict[str, str]:
+    basic_user = config("NOVA_USERNAME", default=None)
+    basic_pwd = config("NOVA_PASSWORD", default=None)
+    if basic_user is not None and basic_pwd is not None:
+        return HTTPBasicAuth(basic_user, basic_pwd)
     return None
